a9622b0d42102f50c75d0553c2d5f1fe5f03e644
Hans Roman <hans.r.69@gmail.com>
2014-08-14 06:51:57 -0500

    Search returns a struct
    - Add Results, Resul & Metadata structs
    - Captcha stuf moved to its own function
    - getErrorMessage becomes has error wich returns bool

--- request.go
+++ request.go
@@ -10,6 +10,8 @@ import (
 	"net/http/cookiejar"
 	"net/url"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"time"
 
@@ -17,27 +19,85 @@ import (
 )
 
 const (
-	search_url  = "http://www.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias"
-	captcha_url = "http://www.sunat.gob.pe/cl-ti-itmrconsruc/captcha?accion=image"
-	ruc_url     = "http://www.sunat.gob.pe/w/wapS01Alias?ruc="
+	SearchURL  = "http://www.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias"
+	CaptchaURL = "http://www.sunat.gob.pe/cl-ti-itmrconsruc/captcha?accion=image"
+	DetailURL  = "http://www.sunat.gob.pe/w/wapS01Alias?ruc="
+	PagingURL  = "http://www.sunat.gob.pe/cl-ti-itmrconsruc/datosRazSoc"
+)
+
+var (
+	ErrInvalidRUC       = errors.New("Invalid RUC")
+	ErrNonexistentRUC   = errors.New("RUC doesn't exist")
+	ErrUnexpected       = errors.New("Unexpected error")
+	ErrSUNAT            = errors.New("Unexpected error from SUNAT")
+	ErrUnsupportedValue = errors.New("Unsupported value")
+	ErrCaptcha          = errors.New("Captcha error")
 )
 
 var timeout = time.Duration(10 * time.Second)
+
+type Result struct {
+	Ruc      string `json:"ruc"`
+	Name     string `json:"name"`
+	Location string `json:"location"`
+	Status   string `json:"status"`
+}
+
+type Metadata struct {
+	Total   int `json:"total"`
+	PerPage int `json:"per_page"`
+	Page    int `json:"page"`
+}
+
+type Results struct {
+	Results  []Result `json:"results"`
+	Metadata Metadata `json:"_meta"`
+}
 
 func dialTimeout(network, addr string) (net.Conn, error) {
 	return net.DialTimeout(network, addr, timeout)
 }
 
-func getErrorMessage(doc *goquery.Document) string {
+func hasError(doc *goquery.Document) bool {
 	result := doc.Find("p.error").First()
-	text := ""
 	if result.Length() > 0 {
-		text = result.Text()
+		text := strings.TrimSpace(result.Text())
+		log.Print(text)
+		return text != ""
 	}
-	return strings.TrimSpace(text)
+	return false
 }
 
-func Search(q string) ([]map[string]string, error) {
+func guessCaptcha(client *http.Client) (string, error) {
+	resp, err := client.Get(CaptchaURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	tmpfile, err := ioutil.TempFile("/tmp", "img")
+	if err != nil {
+		return "", err
+	}
+	defer tmpfile.Close()
+
+	if _, err := io.Copy(tmpfile, resp.Body); err != nil {
+		return "", err
+	}
+
+	captcha := captchaToText(tmpfile.Name())
+	if err := os.Remove(tmpfile.Name()); err != nil {
+		return "", err
+	}
+
+	if captcha == "" {
+		return "", errors.New("Could not recognize image.")
+	}
+
+	return captcha, nil
+}
+
+func Search(q string) (*Results, error) {
 	postData := url.Values{}
 
 	if isDni(q) {
@@ -51,7 +111,7 @@ func Search(q string) ([]map[string]string, error) {
 		postData.Set("accion", "consPorRazonSoc")
 		postData.Set("razSoc", q)
 	} else {
-		return nil, errors.New("Value not supported.")
+		return nil, ErrUnsupportedValue
 	}
 
 	transport := http.Transport{
@@ -64,43 +124,23 @@ func Search(q string) ([]map[string]string, error) {
 		Transport: &transport,
 	}
 
-	resp, err := client.Head(search_url)
+	resp, err := client.Head(SearchURL)
 	if err != nil {
 		log.Print(err)
-		return nil, err
+		return nil, ErrSUNAT
 	}
 	defer resp.Body.Close()
 
-	resp1, err := client.Get(captcha_url)
+	captcha, err := guessCaptcha(client)
 	if err != nil {
 		log.Print(err)
-		return nil, err
+		return nil, ErrUnexpected
 	}
-	defer resp1.Body.Close()
 
-	tmpfile, err := ioutil.TempFile("/tmp", "img")
-	if err != nil {
-		log.Print(err)
-		return nil, err
-	}
-	defer tmpfile.Close()
-
-	if _, err := io.Copy(tmpfile, resp1.Body); err != nil {
-		log.Print(err)
-		return nil, err
-	}
-	captcha := captchaToText(tmpfile.Name())
-	if err := os.Remove(tmpfile.Name()); err != nil {
-		log.Print(err)
-		return nil, err
-	}
-	if captcha == "" {
-		return nil, errors.New("Could not recognize image.")
-	}
 	postData.Set("codigo", captcha)
 	postData.Set("contexto", "ti-it")
 
-	resp2, err := client.PostForm(search_url, postData)
+	resp2, err := client.PostForm(SearchURL, postData)
 	if err != nil {
 		log.Print(err)
 		return nil, err
@@ -112,31 +152,43 @@ func Search(q string) ([]map[string]string, error) {
 		return nil, err
 	}
 
-	if errorText := getErrorMessage(doc); errorText != "" {
-		return nil, errors.New(errorText)
+	if hasError(doc) {
+		return nil, ErrSUNAT
 	}
 
 	rows := doc.Find("td.beta table tr")
 
+	results := &Results{}
+
 	// length includes the "table header"
 	length := rows.Length()
+
 	if length < 2 {
-		log.Print("There are no results.")
-		return nil, nil
+		results.Results = make([]Result, 0)
+	} else {
+		re := regexp.MustCompile("(\\d+)$")
+
+		total, err := strconv.Atoi(re.FindString(doc.Find("td.lnk7").First().Text()))
+		if err != nil {
+			total = 0
+		}
+		results.Metadata.Total = total
+		results.Metadata.PerPage = 30
+
+		rows.Slice(1, length).Each(func(i int, s *goquery.Selection) {
+			cols := s.Find("td")
+			result := Result{
+				// RUC
+				strings.TrimSpace(cols.Eq(0).Find("a").Text()),
+				// Name
+				strings.TrimSpace(cols.Eq(1).Text()),
+				// Location
+				strings.TrimSpace(cols.Eq(2).Text()),
+				// Status
+				strings.TrimSpace(cols.Eq(3).Text()),
+			}
+			results.Results = append(results.Results, result)
+		})
 	}
-
-	data := make([]map[string]string, length-1)
-	rows.Slice(1, length).Each(func(i int, s *goquery.Selection) {
-		cols := s.Find("td")
-		data[i] = map[string]string{
-			"ruc":      strings.TrimSpace(cols.Eq(0).Find("a").Text()),
-			"name":     strings.TrimSpace(cols.Eq(1).Text()),
-			"location": strings.TrimSpace(cols.Eq(2).Text()),
-			"status":   strings.TrimSpace(cols.Eq(3).Text()),
-		}
-	})
-	return data, nil
-}
-
-		}
+	return results, nil
 }
